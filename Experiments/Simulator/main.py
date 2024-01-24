248bbe41bc3ba66864b40c101d7f26c67432a011
Antongiacomo Polimeno <antongiacomo.polimeno@unimi.it>
2024-01-24 12:31:41 +0100

    added chart

--- Experiments/Simulator/main.py
+++ Experiments/Simulator/main.py
@@ -23,37 +23,37 @@ def run_combination(combinazione):
 if __name__ == "__main__":
 
     times = []
-    for e_nodes in range(7, 10):
-        configuration.NUMBER_OF_NODES = e_nodes
-        for e_window in range(2, configuration.NUMBER_OF_NODES+1):
-            configuration.WINDOW_SIZE = 2
-            for e_servies in range(2, 10):
-                configuration.NUMBER_OF_SERVICES = e_servies
-                start_time = time.time()
+    e_nodes= 6
+    configuration.NUMBER_OF_NODES = e_nodes
+    for e_window in range(5, e_nodes+1):
+        configuration.WINDOW_SIZE = e_window
+        for e_servies in range(2, 9):
+            configuration.NUMBER_OF_SERVICES = e_servies
+            start_time = time.time()
 
-                nodelist = NodeList(window_size=configuration.WINDOW_SIZE)
-                for i in range(0, configuration.NUMBER_OF_NODES):
-                    node = Node(i)
+            nodelist = NodeList(window_size=configuration.WINDOW_SIZE)
+            for i in range(0, configuration.NUMBER_OF_NODES):
+                node = Node(i)
 
-                    for j in range(0, configuration.NUMBER_OF_SERVICES):
-                        service = Service(int(f"{i}{j}"), parent=node)  # TODO: change this
-                        node.add_service(service)
-                    nodelist.add(node)
+                for j in range(0, configuration.NUMBER_OF_SERVICES):
+                    service = Service(int(f"{i}{j}"), parent=node)  # TODO: change this
+                    node.add_service(service)
+                nodelist.add(node)
 
-                nodelist.run(data, data_logger)
-                end_time = time.time()
+            nodelist.run(data, data_logger)
+            end_time = time.time()
 
-                print(f"EXECUTION TIME {configuration.NUMBER_OF_NODES} {e_servies}: {end_time - start_time}")
-                with open('event_window_2.csv', 'a') as f_object:
-                    lista = [configuration.NUMBER_OF_NODES,
-                    e_servies,
-                    e_window,
-                    end_time - start_time
-                    ]
-                    writer_object = writer(f_object)
-                    writer_object.writerow(lista)
-                f_object.close()
+            print(f"EXECUTION TIME w{e_window} n{e_nodes} s{e_servies}: {end_time - start_time}")
+            with open(f'event_newwindown_n{e_nodes}_w{e_window}.dat', 'a') as f_object:
+                lista = [e_nodes,
+                e_servies,
+                e_window,
+                end_time - start_time
+                ]
+                writer_object = writer(f_object, delimiter=' ')
+                writer_object.writerow(lista)
+            f_object.close()
 
-                times.append(dict)
+            times.append(dict)
 
-                data_logger.store(f'stats_s{configuration.NUMBER_OF_SERVICES}n{configuration.NUMBER_OF_NODES}.csv')
+            data_logger.store(f'stats_s{configuration.NUMBER_OF_SERVICES}n{configuration.NUMBER_OF_NODES}.csv')
---
+++ Images/graphs/event_newwindow.gp
@@ -0,0 +1,42 @@
+set terminal postscript eps enhanced
+set output 'window_performance.eps'
+
+set ylabel "Computation Time (ms)"
+set xlabel "No. of ingested records"
+base_path = '../../Experiments/Simulator/'
+
+set yrange[0:4096]
+set key box top left inside Left samplen 1
+#set logscale y 2
+
+set size 1,1
+set origin 0,0
+set multiplot layout 2,2 rowsfirst scale 1.1,0.9
+
+set xlabel "Window Size 2"
+plot  base_path.'event_newwindown_n3_w2.dat' u 2:4 t '3 Nodes' w lp pt 6, \
+      base_path.'event_newwindown_n4_w2.dat' u 2:4 t '4 Nodes ' w lp pt 7, \
+      base_path.'event_newwindown_n5_w2.dat' u 2:4 t '5 Nodes' w lp pt 8, \
+      base_path.'event_newwindown_n6_w2.dat' u 2:4 t '6 Nodes ' w lp pt 9, \
+      base_path.'event_newwindown_n7_w2.dat' u 2:4 t '7 Nodes ' w lp pt 10
+
+set xlabel "Window Size 3"
+plot   base_path.'event_newwindown_n3_w3.dat' u 2:4 t '3 Nodes' w lp pt 6, \
+       base_path.'event_newwindown_n4_w3.dat' u 2:4 t '4 Nodes' w lp pt 7, \
+       base_path.'event_newwindown_n5_w3.dat' u 2:4 t '5 Nodes' w lp pt 8, \
+       base_path.'event_newwindown_n6_w3.dat' u 2:4 t '6  Nodes' w lp pt 9, \
+       base_path.'event_newwindown_n7_w3.dat' u 2:4 t '7 Nodes ' w lp pt 10
+
+set xlabel "Window Size 4"
+plot   base_path.'event_newwindown_n4_w4.dat' u 2:4 t '4 Nodes' w lp pt 7, \
+       base_path.'event_newwindown_n5_w4.dat' u 2:4 t '5 Nodes' w lp pt 8, \
+       base_path.'event_newwindown_n6_w4.dat' u 2:4 t '6 Nodes ' w lp pt 9, \
+       base_path.'event_newwindown_n7_w4.dat' u 2:4 t '7 Nodes' w lp pt 10
+
+set xlabel "Window Size 5"
+plot   base_path.'event_newwindown_n5_w5.dat' u 2:4 t '5 Nodes' w lp pt 8, \
+       base_path.'event_newwindown_n6_w5.dat' u 2:4 t '6 Nodes' w lp pt 9, \
+       base_path.'event_newwindown_n7_w5.dat' u 2:4 t '7 Nodes ' w lp pt 10
+
+
+unset multiplot
